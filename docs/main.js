691810238b8f941685514daee5d9d71d9991717b
Solomon Zelenko <zelenkosolomon@gmail.com>
2019-01-22 02:26:38 -0700

    phase out cursor tail-image for h1 and menu

--- docs/main.js
+++ docs/main.js
@@ -84,7 +84,7 @@ let cursorArray = ['media/cursor/cur1.svg',
 'media/cursor/cur16.svg',
 'media/cursor/cur17.svg']; 
 // array of animation frames
-
+ 
 let circle = document.createElement('img');
 // initialize & append image in DOM
 document.body.appendChild(circle);
@@ -92,7 +92,7 @@ circle.setAttribute('class', 'cursor');
 circle.style.position = 'absolute';
 // set attribute for easier reference
 // and set image position to absolute 
- 
+  
 circle.style.transition = 'all 0.1s ease';
 circle.style['-webkit-transition'] = 'all 0.1s ease';
 circle.style['-moz-transition'] = 'all 0.1s ease';
@@ -137,7 +137,82 @@ document.onmousemove = function position(e) {
 // position the image relative to the location of the cursor movement event
 // call the animate circle function
 // pass the locally redefined circle variable through the function 
+let a = document.querySelector('a'); 
+a.onmouseover = function position2(e) {
+    let circle = document.querySelector('.cursor');
+    circle.style.transform = 'scaleX(0) scaleY(0)';
+    circle.style['-webkit-transform'] = 'scaleX(0) scaleY(0)';
+    circle.style['-moz-transform'] = 'scaleX(0) scaleY(0)';
+    circle.style['-ms-transform'] = 'scaleX(0) scaleY(0)';
+    circle.style['-o-transform'] = 'scaleX(0) scaleY(0)';
+    circle.style.transition = 'transform 0.3s ease';
+    circle.style['-webkit-transition'] = '-webkit-transform 0.3s ease';
+    circle.style['-moz-transition'] = '-moz-transform 0.3s ease';
+    circle.style['-ms-transition'] = '-ms-transform 0.3s ease'; 
+    circle.style['-o-transition'] = '-o-transform 0.3s ease';
+}
+a.onmouseout = function position3(e) {
+    let circle = document.querySelector('.cursor');
+    circle.style.transform = 'scaleX(1) scaleY(1)';
+    circle.style['-webkit-transform'] = 'scaleX(1) scaleY(1)';
+    circle.style['-moz-transform'] = 'scaleX(1) scaleY(1)';
+    circle.style['-ms-transform'] = 'scaleX(1) scaleY(1)';
+    circle.style['-o-transform'] = 'scaleX(1) scaleY(1)';
+    circle.style.transition = 'transform 0.3s ease';
+    circle.style['-webkit-transition'] = '-webkit-transform 0.3s ease';
+    circle.style['-moz-transition'] = '-moz-transform 0.3s ease';
+    circle.style['-ms-transition'] = '-ms-transform 0.3s ease'; 
+    circle.style['-o-transition'] = '-o-transform 0.3s ease';
+
+}  
+let menu = document.querySelector('.menu--expanded'); 
+menu.onmouseover = function position4(e) {
+    let circle = document.querySelector('.cursor');
+    circle.style.transform = 'scaleX(0) scaleY(0)';
+    circle.style['-webkit-transform'] = 'scaleX(0) scaleY(0)';
+    circle.style['-moz-transform'] = 'scaleX(0) scaleY(0)';
+    circle.style['-ms-transform'] = 'scaleX(0) scaleY(0)';
+    circle.style['-o-transform'] = 'scaleX(0) scaleY(0)';
+    circle.style.transition = 'transform 0.3s ease';
+    circle.style['-webkit-transition'] = '-webkit-transform 0.3s ease';
+    circle.style['-moz-transition'] = '-moz-transform 0.3s ease';
+    circle.style['-ms-transition'] = '-ms-transform 0.3s ease'; 
+    circle.style['-o-transition'] = '-o-transform 0.3s ease';
+} 
+menu.onmouseout = function position5(e) {
+    let circle = document.querySelector('.cursor');
+    circle.style.transform = 'scaleX(1) scaleY(1)';
+    circle.style['-webkit-transform'] = 'scaleX(1) scaleY(1)';
+    circle.style['-moz-transform'] = 'scaleX(1) scaleY(1)';
+    circle.style['-ms-transform'] = 'scaleX(1) scaleY(1)';
+    circle.style['-o-transform'] = 'scaleX(1) scaleY(1)';
+    circle.style.transition = 'transform 0.3s ease';
+    circle.style['-webkit-transition'] = '-webkit-transform 0.3s ease';
+    circle.style['-moz-transition'] = '-moz-transform 0.3s ease';
+    circle.style['-ms-transition'] = '-ms-transform 0.3s ease'; 
+    circle.style['-o-transition'] = '-o-transform 0.3s ease';
+} 
  
+/*
+    transform: scaleX(2.5);
+    -webkit-transition: all 0.5s ease;
+    -moz-transition:all 0.5s ease;
+    -ms-transition:all 0.5s ease;
+    -o-transition:all 0.5s ease;
+    transition:all 0.5s ease;  
+
+    circle.style.transform = 'scaleX(2) scaleY(2)';
+    circle.style['-webkit-transform'] = 'scaleX(2) scaleY(2)';
+    circle.style['-moz-transform'] = 'scaleX(2) scaleY(2)';
+    circle.style['-ms-transform'] = 'scaleX(2) scaleY(2)';
+    circle.style['-o-transform'] = 'scaleX(2) scaleY(2)';
+    circle.style.transition = 'all 0.1s ease';
+    circle.style['-webkit-transition'] = 'all 0.1s ease';
+    circle.style['-moz-transition'] = 'all 0.1s ease';
+    circle.style['-ms-transition'] = 'all 0.1s ease'; 
+    circle.style['-o-transition'] = 'all 0.1s ease';
+*/
+
 
 
  
